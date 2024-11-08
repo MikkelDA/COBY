d15c1dccbc262400ad44898883a22e4abe3f6e28
Mikkel Dahl Andreasen <116167950+MikkelDA@users.noreply.github.com>
2024-11-08 12:36:40 +0100

    Delete COBY directory

--- COBY/structure_classes/PROTEIN_class.py
+++
@@ -1,5 +0,0 @@
-from COBY.structure_classes.MOLECULE_class import MOLECULE
-
-class PROTEIN(MOLECULE):
-    def __init__(self, molname = False, moleculetype = False):
-        super().__init__(molname = molname, moleculetype = False)
